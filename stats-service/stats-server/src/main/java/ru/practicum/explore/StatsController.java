f7835963fcc1f0bfe12f68bad4973da0d313206f
Igor <igorilizian@yandex.ru>
2023-07-30 18:42:46 +0400

    fix: исправление по замечаниям

--- stats-service/stats-server/src/main/java/ru/practicum/explore/StatsController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/explore/StatsController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explore.exception.ValidationException;
 
